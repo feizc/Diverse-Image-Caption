a61217a557df5930f7861d1c3f1c5889dd0cd9bc
费政聪 <3300949577@qq.com>
2021-12-26 20:39:22 +0800

    1. inference accelerating 2. image size debug

--- feature_extraction.py
+++ feature_extraction.py
@@ -7,7 +7,8 @@ import copy
 import torchvision.models as models
 import torch.nn as nn
 import numpy as np 
-import pickle 
+import pickle
+from torchvision.models import detection 
 from tqdm import tqdm 
 
 
@@ -18,6 +19,8 @@ ssl._create_default_https_context = ssl._create_unverified_context
 def image_feature_extract(image_data_path, feature_saved_path):
     image_name_list = os.listdir(image_data_path) 
     
+    use_device = torch.cuda.is_available()
+    device = torch.device('cuda' if use_device else 'cpu') 
     transform = transforms.ToTensor() 
     norm_mean = [0.485, 0.456, 0.406]
     norm_std = [0.229, 0.224, 0.225]
@@ -29,18 +32,22 @@ def image_feature_extract(image_data_path, feature_saved_path):
 
     resnext50_32x4d = models.resnext101_32x8d(pretrained=True) 
     feature_extractor = nn.Sequential(*(list(resnext50_32x4d.children())[:-1]))
+    feature_extractor = feature_extractor.to(device)
 
+    detection_model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True) 
+    detection_model.eval() 
+    detection_model.to(device)
     image_features_dict = {}
     
     for image_name in tqdm(image_name_list): 
         image_path = os.path.join(image_data_path, image_name) 
         image_PIL = Image.open(image_path).convert('RGB') 
+        width, height = image_PIL.size
         # image_PIL.show() 
         # https://blog.csdn.net/tsq292978891/article/details/78767326 
-        image_tensor = transform(image_PIL) 
+        image_tensor = transform(image_PIL).to(device) 
+        
         # https://blog.csdn.net/yanxiangtianji/article/details/112256618
-        detection_model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True) 
-        detection_model.eval() 
         predictions = detection_model([image_tensor]) 
         boxes = predictions[0]['boxes']
         # plot the extracted objective 
@@ -51,9 +58,15 @@ def image_feature_extract(image_data_path, feature_saved_path):
         
         image_feature_list = []
 
-        for box in boxes[:2]: 
-            image_crop = image_PIL.crop(tuple(box.tolist()))
-            image_crop = crop_transform(image_crop)
+        for box in boxes[:5]: 
+            box = box.tolist() 
+            x1 = max(int(box[0]), 0) 
+            y1 = max(int(box[1]), 0)
+            x2 = min(int(box[2]), width)
+            y2 = min(int(box[3]), height)
+            image_crop = image_PIL.crop(tuple([x1, y1, x2, y2])) 
+            # image_crop.show()
+            image_crop = crop_transform(image_crop).to(device) 
             image_crop.unsqueeze_(0)
             image_feature = feature_extractor(image_crop).squeeze(2).squeeze(2)[0] 
             image_feature_list.append(image_feature.tolist()) 
@@ -66,6 +79,6 @@ def image_feature_extract(image_data_path, feature_saved_path):
 
 
 if __name__ == '__main__': 
-    train_image_path = 'data/images/train2014'  
-    feature_saved_path = 'data/features'
+    train_image_path = 'dataset/images/train2014'  
+    feature_saved_path = 'dataset/features'
     image_feature_extract(train_image_path, feature_saved_path)
